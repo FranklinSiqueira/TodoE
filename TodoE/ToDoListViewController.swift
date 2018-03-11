d2e5e839b7704cb9c80c7b25c078b98ff347744b
Franklin Siqueira <franklin.siqueira@me.com>
2018-03-11 15:32:36 -0400

    First commit to remote with alterations made as per 03/11/2018.

---
+++ TodoE/ToDoListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  TodoE
+//
+//  Created by Franklin Siqueira on 09/03/18.
+//  Copyright © 2018 Franklin Siqueira. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["Todo One", "Todo Two", "Todo Three"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Adding a check mark
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+    }
+    
+    
+}
+
+
+
+
+
+
+
