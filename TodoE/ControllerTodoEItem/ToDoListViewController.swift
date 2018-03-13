4aac72e4f4c838f0389a285efc3e05519aa91dca
Franklin Siqueira <franklin.siqueira@me.com>
2018-03-12 21:19:04 -0400

    Corrected the problem that made App crash ($@ instead of %@) due to failure to parse data inserted on searchBar.

--- TodoE/ControllerTodoEItem/ToDoListViewController.swift
+++ TodoE/ControllerTodoEItem/ToDoListViewController.swift
@@ -13,41 +13,16 @@ import CoreData
 
 class ToDoListViewController: UITableViewController {
 
-    // Defining data paths
-    // let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
-    
     // Creating an array of objects of class item, defined in item.swift
     var itemArray = [Item]()
-    
-    // Default settings variable
-    // let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Retrieving data into plist file (if it exists)
-        // if let items = defaults.array(forKey: "ToDoListArray") as? [Item]{
-        //    itemArray = items
-        //}
-        
-        // Using newly defined hard coded data type item
-//        let newItem = Item()
-//        newItem.title = "Go to dentist"
-//        itemArray.append(newItem)
-        
         loadItems()
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-        
-        // Delegating searchBar
-        // searchBar.delegate = self
-        
-        
     }
-    
-    //@IBOutlet weak var searchBar: UISearchBar!
     
     //MARK: - TableView DataSource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -68,25 +43,12 @@ class ToDoListViewController: UITableViewController {
     //MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
-        
-        // If one wants to phisically delete items from database. Order MATTERS!
-//        context.delete(itemArray[indexPath.row])
-//        itemArray.remove(at: indexPath.row)
-        
+    
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
-        
-//        tableView.deselectRow(at: indexPath, animated: true)
-//        // Adding a check mark
-//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        } else {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        }
-        
     }
     
     // MARK: - Add New Items
@@ -108,7 +70,6 @@ class ToDoListViewController: UITableViewController {
             // Calling saveItems() to append data
             self.saveItems()
             
-            //self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Insert New Item"
@@ -152,14 +113,12 @@ extension ToDoListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest()
         
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] $@", searchBar.text!)
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
         loadItems(with: request)
     }
-    
-//    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
 
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0 {
